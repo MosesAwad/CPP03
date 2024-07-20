dec3e6bfbf5189e051cb8af809ac7a7f0add7a7e
Moosa Awad <mawad@lab1r3s8.42abudhabi.ae>
2024-07-21 01:26:16 +0400

    Wrote neater mains

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	ClapTrap Lame("Lame");
 
+	std::cout << "\n---Testing member functions on ClapTrap object Lame---\n" << std::endl;
 	Lame.takeDamage(10);
 	Lame.beRepaired(99);
 	Lame.beRepaired(1);
@@ -13,17 +14,20 @@ int main()
 	// Lame2 takes all the values of the ClapTrap attributes from Lame
 	// at the time of copying but it then becomes an independent object.
 	// So Lame2 taking damage does not mean that Lame takes damage.
+	std::cout << "\n---Testing the copy constructor on ClapTrap object Lame2 by copying ClapTrap object Lame---\n" << std::endl;
 	ClapTrap Lame2(Lame);
 	Lame2.takeDamage(1);
 	Lame.takeDamage(1);
 
 	Lame2.attack("Loser");
 
+	std::cout << "\n---Testing the assignment operator on ClapTrap object Lame3 by copying ClapTrap object Lame2---\n" << std::endl;
 	ClapTrap Lame3("Lame3");
 	Lame3 = Lame2;
 	Lame3.takeDamage(1);
 	Lame2.takeDamage(1);
 
-
+	std::cout << "\n---Time for Clappy bye bye XD---\n" << std::endl;
 	return (0);
 }
+
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,33 +1,38 @@
 
 #include "ScavTrap.hpp"
 
+
 int main()
 {
 	/*
 		MAIN 1
 	*/
-	std::cout << "---Main 1---" << std::endl;
+	std::cout << "------------------Main 1------------------" << std::endl;
 	{
+		std::cout << "\n---Testing ScavTrap member functions---\n" << std::endl;
 		ScavTrap Lame("Lame");
 
 		Lame.takeDamage(4);
 		Lame.attack("Stupid");
 
+		std::cout << "\n---Testing ScavTrap copy constructor by copying ScavTrap object Lame into new ScavTrap object Lame2---\n" << std::endl;
 		ScavTrap Lame2(Lame);
 		Lame2.beRepaired(3);
 		Lame2.guardGate();
+
+		std::cout << "\n---Time for Scavvy bye bye XD---\n" << std::endl;
 	}
 	/*
 		MAIN 2
 	*/
-	std::cout << "---Main 2---" << std::endl;
+	std::cout << "\n\n------------------Main 2------------------" << std::endl;
 	{
-		ClapTrap *Bane;
+		std::cout << "\n---Testing ScavTrap compatibility with polymorphism on the attack function and the destructor---\n" << std::endl;
 
+		ClapTrap *Bane;
 		Bane = new ScavTrap("Bane");
 
 		Bane->attack("Idiot");
-
 		delete(Bane);
 	}
 	return (0);
